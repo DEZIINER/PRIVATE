dd082794c9f14ffd3b043906cab190227868b2d1
DEZIINER <127255670+DEZIINER@users.noreply.github.com>
2023-09-08 16:27:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,8 +26,8 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001638006524').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1058015838').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '-1001521700370')
-auth_grp = environ.get('AUTH_GROUP', '-1001806026278')
+auth_channel = environ.get('AUTH_CHANNEL', ' -1001859057505')
+auth_grp = environ.get('AUTH_GROUP', '-1001926360406')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
