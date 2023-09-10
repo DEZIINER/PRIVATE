58273e37ff060a500d6e6ef5a142bad92e74f734
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-09-10 16:33:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,6 +37,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'premium')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001652564383'))
+DELETE = environ.get('DELETE', "60")
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MYFLiiX_2')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
