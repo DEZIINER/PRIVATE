16d8f8cf8516158c438e6d0a2cb7b97dc674ede0
DEZIINER <127255670+DEZIINER@users.noreply.github.com>
2023-09-12 19:09:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME, temp.greeting),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
