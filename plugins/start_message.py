5b9265c4b1bf681c1de602e674ef273c3d8f8242
DEZIINER <127255670+DEZIINER@users.noreply.github.com>
2023-09-12 20:23:42 +0530

    Create start_message.py

---
+++ plugins/start_message.py
@@ -0,0 +1,19 @@
+import datetime
+
+@bot.command("start")
+def start_command(update, context):
+    user_id = update.message.from_user.id
+    current_time = datetime.datetime.now()
+    greeting = get_greeting(current_time.hour)
+    context.bot.send_message(chat_id=user_id, text=f"Good {greeting}! Welcome to my bot. How can I assist you?")
+
+def get_greeting(hour):
+    if 5 <= hour < 12:
+        return "Morning"
+    elif 12 <= hour < 17:
+        return "Afternoon"
+    elif 17 <= hour < 21:
+        return "Evening"
+    else:
+        return "Night"
+      
